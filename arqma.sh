621c22bd5b245eecdc691d82f6f822aa0a6b7abb
GreggyGB <43723411+GreggyGB@users.noreply.github.com>
2021-10-07 11:12:13 +0100

    Update arqma.sh

--- arqma.sh
+++ arqma.sh
@@ -9,9 +9,9 @@ install_node () {
   sudo apt install wget unzip
   sudo apt update
   sudo apt-get install  build-essential cmake pkg-config libboost-all-dev libssl-dev libzmq3-dev libunbound-dev libsodium-dev libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev doxygen graphviz libpgm-dev qttools5-dev-tools libhidapi-dev libusb-dev libprotobuf-dev protobuf-compiler
+  git submodule update --init --force external/libzmq
   git clone --recursive 'https://github.com/malbit/arqma' arqma && cd arqma
   git submodule init && git submodule update
-  git submodule update --init --force external/libzmq
   git checkout s_nodes
   make
 
